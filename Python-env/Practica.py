5b28275b5e30b82a69f81361b43d603a582f6cca
tenshi <Angel_coronado03@hotmail.com>
2026-03-19 22:22:35 -0600

    M fume un cigarro y ya jalo

--- Python-env/Practica.py
+++ Python-env/Practica.py
@@ -1,55 +1,23 @@
-import colorama
-
-colorama.init()
-
-rojo = colorama.Fore.RED
-azul = colorama.Fore.BLUE
-verde = colorama.Fore.GREEN
-amarillo = colorama.Fore.YELLOW
-naranja = colorama.Fore.LIGHTRED_EX
-reset = colorama.Style.RESET_ALL
-
-print("Bienvenido al seleccionador")
 
 while True:
-    intro = int(input("Ingrese un numero (0 para salir): "))
-    
-    if intro == 0:
-        print("Saliendo del programa...")
+    print("Bienvenido, esto solo es una practica simple de python")
+    opcion = input("Presiona 1 para continuar o cualquier otra tecla para salir: ")
+    if opcion != "1":
+        Texto = input("Escribe lo que sea: ")
+        print("selecciona un color para el texto: ")
+        print("1. Rojo")
+        print("2. Verde")
+        print("3. Cyan")
+        color_texto = int(input("Ingresa el número del color: "))
+        if color_texto == 1:
+            print("\033[31m" + Texto + "\033[0m")
+        elif color_texto == 2:
+            print("\033[32m" + Texto + "\033[0m")       
+        elif color_texto == 3:
+            print("\033[36m" + Texto + "\033[0m")
+        else:        
+            print("Opción de color no válida. Mostrando texto sin color.")
+            print(Texto)
+    else:
+        print("¡Gracias por usar esta practica de python! ¡Hasta luego!")
         break
-
-    if 1 <= intro <= 10:
-        print("Seleccionaste el numero " + str(intro))
-    else:
-        print(rojo + "Ingrese un numero valido" + reset)
-        continue
-
-    print("\nSelecciona un color:")
-    print("1. Rojo")
-    print("2. Azul")    
-    print("3. Verde")
-    print("4. Amarillo")
-    print("5. Naranja")
-
-    color = int(input("Ingrese un color: "))
-
-    if color == 1:
-        print(rojo + "Seleccionaste Rojo" + reset)
-        color_nombre = "Rojo"
-    elif color == 2:
-        print(azul + "Seleccionaste Azul" + reset)
-        color_nombre = "Azul"
-    elif color == 3:
-        print(verde + "Seleccionaste Verde" + reset)
-        color_nombre = "Verde"
-    elif color == 4:
-        print(amarillo + "Seleccionaste Amarillo" + reset)
-        color_nombre = "Amarillo"
-    elif color == 5:
-        print(naranja + "Seleccionaste Naranja" + reset)
-        color_nombre = "Naranja"
-    else:
-        print(rojo + "Color no valido" + reset)
-        continue
-
-    print("Felicidades, ahora tienes un superduper " + color_nombre + " " + str(intro))
